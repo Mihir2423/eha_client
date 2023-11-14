4fc6d14f23e409a8628bf68624f0e7507524a300
Mihir2423 <mihirraj444@gmail.com>
2023-11-14 10:24:09 +0530

    fixed fetching api

--- pages/api/products.js
+++ pages/api/products.js
@@ -4,9 +4,7 @@ export default async function handler(req, res) {
     const apiRes = await fetch(`${process.env.NEXT_PUBLIC_NEXT_API_PUBLIC_URL}/api/products?populate=*`);
     const apiData = await apiRes.json();
     
-    const filteredItems = apiData?.data?.filter(
-      (item) => item.attributes.category.data.attributes.name === "laptops"
-    );
+    const filteredItems = apiData?.data
 
     res.status(200).json({ filteredItems });
   } catch (error) {
